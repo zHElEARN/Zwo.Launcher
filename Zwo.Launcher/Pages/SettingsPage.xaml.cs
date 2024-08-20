eacbbac2f9e6be9a76b3505c435b39b252363368
zHElEARN <personal@zhelearn.com>
2024-08-21 00:13:57 +0800

    feat: add configuration file read, write, and save functionality

--- Zwo.Launcher/Pages/SettingsPage.xaml.cs
+++ Zwo.Launcher/Pages/SettingsPage.xaml.cs
@@ -7,11 +7,13 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Zwo.Launcher.Utils;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +28,31 @@ namespace Zwo.Launcher.Pages
         public SettingsPage()
         {
             this.InitializeComponent();
+
+            ConfigurationManager.LoadConfiguration();
+
+            ConfigurationModeComboBox.SelectedIndex = ConfigurationManager.Config.ConfigurationMode == "auto" ? 0 : 1;
+
+            ProxySettingsToggleSwitch.IsOn = ConfigurationManager.Config.ProxySettings.IsEnabled;
+            UseSystemSettingsToggleSwitch.IsOn = ConfigurationManager.Config.ProxySettings.IsUseSystemSettings;
+            ProxyServerAddressTextBox.Text = ConfigurationManager.Config.ProxySettings.ProxyServerAddress;
+
+            DownloadAccelerationToggleSwitch.IsOn = ConfigurationManager.Config.DownloadAcceleration.IsEnabled;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            ConfigurationManager.Config.ConfigurationMode = ConfigurationModeComboBox.SelectedIndex == 0 ? "auto" : "manual";
+            
+            ConfigurationManager.Config.ProxySettings.IsEnabled = ProxySettingsToggleSwitch.IsOn;
+            ConfigurationManager.Config.ProxySettings.IsUseSystemSettings = UseSystemSettingsToggleSwitch.IsOn;
+            ConfigurationManager.Config.ProxySettings.ProxyServerAddress = ProxyServerAddressTextBox.Text;
+
+            ConfigurationManager.Config.DownloadAcceleration.IsEnabled = DownloadAccelerationToggleSwitch.IsOn;
+
+            ConfigurationManager.SaveConfiguration();
         }
     }
 }
---
+++ Zwo.Launcher/Utils/ConfigurationManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Zwo.Launcher.Utils
+{
+    public static class ConfigurationManager
+    {
+        private static readonly string ConfigFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @".zlauncher\config.json");
+
+        public static Configuration Config { get; private set; }
+
+        public static void LoadConfiguration()
+        {
+            try
+            {
+                string configJson = File.ReadAllText(ConfigFilePath);
+                Config = JsonSerializer.Deserialize<Configuration>(configJson);
+            }
+            catch (Exception)
+            {
+                Config = new Configuration();
+            }
+        }
+
+        public static void SaveConfiguration()
+        {
+            string configJson = JsonSerializer.Serialize(Config, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(ConfigFilePath, configJson);
+        }
+    }
+
+    public class Configuration
+    {
+        [JsonPropertyName("configurationMode")]
+        public string ConfigurationMode { get; set; } = "auto";
+
+        [JsonPropertyName("proxySettings")]
+        public ProxySettings ProxySettings { get; set; } = new ProxySettings();
+
+        [JsonPropertyName("downloadAcceleration")]
+        public DownloadAcceleration DownloadAcceleration { get; set; } = new DownloadAcceleration();
+    }
+
+    public class ProxySettings
+    {
+        [JsonPropertyName("enabled")]
+        public bool IsEnabled { get; set; } = false;
+
+        [JsonPropertyName("useSystemSettings")]
+        public bool IsUseSystemSettings { get; set; } = true;
+
+        [JsonPropertyName("proxyServerAddress")]
+        public string ProxyServerAddress { get; set; } = "http://127.0.0.1:7890";
+    }
+
+    public class DownloadAcceleration
+    {
+        [JsonPropertyName("enabled")]
+        public bool IsEnabled { get; set; } = true;
+
+        [JsonPropertyName("mirror")]
+        public string Mirror { get; set; } = "mirror.ghproxy.com";
+    }
+}
